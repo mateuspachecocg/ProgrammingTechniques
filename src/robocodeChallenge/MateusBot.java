e6704694c7d88df1bfcdb491fa7ff48e3645c5b7
Mateus Pacheco <mateuspacheco@alu.ufc.br>
2022-11-02 10:50:50 -0300

    MatesBot first Strategy

--- src/robocodeChallenge/MateusBot.java
+++ src/robocodeChallenge/MateusBot.java
@@ -2,14 +2,27 @@ package robocodeChallenge;
 
 import robocode.*;
 
+/**
+ * MateusBot - A simplest bot maded by Mateus Pacheco.
+ * <p>
+ * The description of the bot will be here!
+ * it will take a momen.
+ * 
+ * @author Mateus Pacheco (original)
+ */
+
 public class MateusBot extends Robot{
 	public void run() {
 		while(true) {
 			ahead(100);
-			turnGunRight(360);
-			back(100);
-			turnGunRight(360);
+			turnRight(60);
+			ahead(100);
+			turnRight(60);
 		}
+	}
+	
+	public void onHitByBullet(HitByBulletEvent e) {
+		turnRight(90 - e.getBearing());
 	}
 	
 	public void onScannedRobot(ScannedRobotEvent e) {
